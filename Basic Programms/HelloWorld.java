16e9d4970c8b91235a0812f86ae27810e432c6a4
AKSHAY MULE <84891959+aksxy@users.noreply.github.com>
2022-05-19 23:19:59 +0530

    Hello World In Java

---
+++ Basic Programms/HelloWorld.java
@@ -0,0 +1,8 @@
+//My first java program Hello world!
+class HelloWorld   // FileName : "HelloWorld.java", give same name as file name to class
+{
+    public static void main(String args[]) //program begins with a call to main().
+    {
+        System.out.println("Hello World!"); //print satetment
+    }
+}
