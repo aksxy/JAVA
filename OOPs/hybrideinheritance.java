9e4c74e4d1a42e3064c15e81a98064ce800af4e4
AKSHAY MULE <84891959+aksxy@users.noreply.github.com>
2022-06-17 15:08:19 +0530

    hybride inheritance

---
+++ OOPs/hybrideinheritance.java
@@ -0,0 +1,34 @@
+//Hybride Inheritance 
+class Subject  // Subject + Total + Display + MultiLevelInheritance            Base class
+{              //Parent   + Big Child + Child  + Public Class                      |
+                                                        //                        / \
+    int mr1,mr2;                                        //                   child   child
+}                                                      //                              |
+class Total extends Subject                               //                          / \
+{     String Subject;                                       //                  child     child
+    void add()                //Total class Inherited Subject class
+    {
+        System.out.println((mr1+mr2));
+    }
+}
+class Display extends Total
+{
+    void Print() //Display class Inherited Total class
+    {
+        System.out.print(Subject);
+    }
+    
+}
+public class MultiLevelInheritance
+{
+    public static void main(String args[])
+    {
+        Total call = new Total();
+        Display call2 =new Display();
+        call.mr1=10;
+        call.mr2=20;
+        call2.Subject="JAVA Total Marks : ";
+        call2.Print();
+        call.add();
+    }
+}
