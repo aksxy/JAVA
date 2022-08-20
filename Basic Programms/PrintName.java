34d2342d644a5ee67bca359d379ca4590369858c
AKSHAY MULE <84891959+aksxy@users.noreply.github.com>
2022-08-21 05:13:47 +0530

    Print Name

---
+++ Basic Programms/PrintName.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class PrintName {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Name : ");
+        String Name = sc.nextLine();
+        System.out.println("Hello " + Name + "!");
+    }
+}
