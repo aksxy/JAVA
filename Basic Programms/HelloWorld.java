76672a2646ab4cb84dbe5eedeb5c2fcb007a4300
AKSHAY MULE <84891959+aksxy@users.noreply.github.com>
2023-01-10 19:33:30 +0530

    DataTypes

--- Basic Programms/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-//My first java program Hello world!
-class HelloWorld   // FileName : "HelloWorld.java", give same name as file name to class
-{
-    public static void main(String args[]) //program begins with a call to main().
-    {
-        System.out.println("Hello World!"); //print satetment
-    }
-}
