b069f18114a5cd411e502858b1ad8ddeee97f4f1
AKSHAY MULE <84891959+aksxy@users.noreply.github.com>
2023-01-11 04:18:46 +0530

    DataTypes

---
+++ CodeChef_Qustions/Basic Programms/DataTypes.java
@@ -0,0 +1,13 @@
+//All Primetive DataTypes of java
+public class DataTypes {
+    public static void main(String[] args) {
+        int number = 89;
+        float Decimalno = 455.362f; // In the float you need to use small (f) after your decimal value
+        char letter = 'a';
+        double lareDecimal = 6532055.16532;
+        long largeInteger = 45631654845631L; // In the long you need to use capital (L) after your value
+        boolean check = true;
+
+        System.out.println(number + "\n" + Decimalno + "\n" + letter + "\n" + lareDecimal + "\n" + largeInteger);
+    }
+}
